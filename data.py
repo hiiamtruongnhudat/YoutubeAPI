25bcb66ac1b199a80349bf949ca19b6554a2219a
hiiamtruongnhudat <dattn.b19at039@stu.ptit.edu.vn>
2021-05-31 05:30:44 +0700

    Work

---
+++ data.py
@@ -0,0 +1,70 @@
+from PyQt5.QtCore import pyqtSignal, QThread
+import os,pickle,re,requests,pytchat
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+class YouTubeAPI(QThread):
+    updateListWidget = pyqtSignal(str)
+    def __init__(self,args, index=0):
+        QThread.__init__(self)
+        self.args = args
+        self.index = index
+    def login(self):
+        SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
+        creds = None
+        path = self.args.ui.lineEdit.text()
+        if os.path.exists("token.pickle"):
+            with open("token.pickle", "rb") as token:
+                creds = pickle.load(token)
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    path, SCOPES
+                )
+                creds = flow.run_local_server(port=0)
+            with open("token.pickle", "wb") as token:
+                pickle.dump(creds, token)
+    def getIDVideo(self):
+        return str(re.findall('v=(.*)',requests.get(self.args.ui.lineEdit_2.text()).url)[0])
+    def getLiveChatID(self):
+        api_key = 'AIzaSyDNchT4xvqof0DpPIjpdsxRHQM4N9MUZts'
+        youtube = build('youtube','v3', developerKey = api_key)
+        res = youtube.videos().list(id=self.getIDVideo(),
+                            part='snippet,contentDetails,liveStreamingDetails').execute()
+        return res.get('items')[0].get('liveStreamingDetails').get('activeLiveChatId')
+    def commentYoutube(self):
+        liveChatId = self.getLiveChatID()
+        with open("token.pickle", "rb") as token:
+            creds = pickle.load(token)
+        youtube = build('youtube', 'v3', credentials=creds)
+        youtube.videos().rate(rating='like', id=self.getIDVideo()).execute()
+        request = youtube.liveChatMessages().insert(
+            part="snippet",
+                body={
+                    "snippet": {
+                    "liveChatId": liveChatId,
+                    "type": "textMessageEvent",
+                    "textMessageDetails": {
+                    "messageText": f"{self.args.ui.lineEdit_3.text()}"
+                    }
+                }
+            }
+        )
+        response = request.execute()
+        # print(response)
+    def getMessageLivechat(self):
+        chat = pytchat.create(video_id=self.getIDVideo())
+        while chat.is_alive():
+            for c in chat.get().sync_items():
+                self.updateListWidget.emit(f"{c.datetime} [{c.author.name}]- {c.message}")
+                print(f"{c.datetime} [{c.author.name}]- {c.message}")
+    def run(self):
+        if self.index == 0:
+            self.login()
+        if self.index == 1:
+            self.getMessageLivechat()
+        if self.index == 2:
+            self.commentYoutube()
+        self.exec_()
---
+++ main.py
@@ -0,0 +1,53 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow,QFileDialog
+from gui import*
+from data import*
+from PyQt5.QtCore import QThread, pyqtSlot
+# https://www.youtube.com/watch?v=tRmrAtth8ls
+class MyApp(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.thread = {}
+        self.ui.pushButton.clicked.connect(self.connectClient)
+        self.ui.pushButton_2.clicked.connect(self.startCrawlMessage)
+        self.ui.pushButton_3.clicked.connect(self.stop)
+        self.ui.pushButton_4.clicked.connect(self.commitComment)
+        self.ui.toolButton.clicked.connect(self.openFile)
+    def connectClient(self):
+        self.thread[1] = YouTubeAPI(self,index=0)
+        self.thread[1].start()
+        self.ui.pushButton.setEnabled(False)
+    def startCrawlMessage(self):
+        self.thread[2] = YouTubeAPI(self,index=1)
+        self.thread[2].updateListWidget.connect(self.updateList)
+        self.thread[2].start()
+        self.ui.pushButton_2.setEnabled(False)
+    def commitComment(self):
+        self.thread[3] = YouTubeAPI(self,index=2)
+        self.thread[3].start()
+        self.ui.pushButton_4.setEnabled(False)
+    def stop(self):
+        if self.thread[1].isRunning():
+            self.thread[1].terminate()
+            self.ui.pushButton.setEnabled(True)
+        if self.thread[2].isRunning():
+            self.thread[2].terminate()
+        if self.thread[3].isRunning():
+            self.thread[3].terminate()
+            self.ui.pushButton_4.setEnabled(True)
+        self.ui.pushButton_2.setEnabled(True)
+    def updateList(self,value):
+        self.ui.listWidget.addItem(value)
+    def openFile(self):
+        folder = QFileDialog.getOpenFileName(None, "Select a file","", filter="All files (*)")[0]
+        self.ui.lineEdit.setText(str(folder))
+        folder = open('options', 'w')
+        folder.write(str(folder))
+        folder.close()
+if __name__ == "__main__":
+    import sys
+    app = QApplication(sys.argv)
+    ui = MyApp()
+    ui.show()
+    sys.exit(app.exec_())
